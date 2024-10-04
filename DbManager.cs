056d1b113a3f011833ff6dfe03bec21fc0b267a2
Mai <mai.than.222@gmail.com>
2024-10-04 12:31:33 +0200

    Methodes et fonctions de base du programme

--- DbManager.cs
+++ DbManager.cs
@@ -39,7 +39,7 @@ namespace DbManager
             Console.WriteLine("Spectateur inséré dans la base !");
         }
         // Supprimer un spectateur de la table spectateur :
-        public void DeleteVisitor(string deleteSpectateur)
+        public void DeleteSpectatorFromDB(string deleteSpectateur)
         {
 
             // Requête pour supprimer un spectateur de la table "spectateur" :
@@ -50,9 +50,9 @@ namespace DbManager
             Console.WriteLine("Vous avez bien supprimé votre spectateur de la base.");
         }
         // Insérer une séance dans la table "liste_seance" :
-        public void AddSeanceToDb(int nom_cinema, string num_salle, string num_seance, string heure_debut, string acces_seance, int id_film)
+        public void AddSeanceToDb(string nom_cinema, int num_salle, int num_seance, string heure_debut, string acces_seance, int id_film)
         {
-            // Requête pour insérer un spectateur VIP dans la table "spectateur" :
+            // Requête pour insérer une seance dans la table "liste_seance" :
             using var command2 = connection.CreateCommand();
             command2.CommandText = @"INSERT INTO `liste_seance`(`nom_cinema`, `num_salle`, `num_seance`, `heure_debut`, `acces_seance`, `id_film`) VALUES (@nom_cinema, @num_salle, @num_seance, @heure_debut, @acces_seance, @id_film)";
             command2.Parameters.AddWithValue("@id_spectateur", nom_cinema);
@@ -66,20 +66,33 @@ namespace DbManager
         }
 
         // Insérer un spectateur avec sa séance dans la table "seance" :
-        public void AddSpectateurToSeanceDb(int nom_cinema, string num_salle, string num_seance, string heure_debut, string acces_seance, int id_film, int id_spectateur)
+        public void AddSpectateurToSeanceDb(string nomCinemaSeance, int numSalleSeance, int numDeSeance, string heureDebutSeance, string accesSeance, int idFilmSeance, int id_spectateurSeance)
         {
             // Requête pour insérer un spectateur VIP dans la table "spectateur" :
             using var command2 = connection.CreateCommand();
             command2.CommandText = @"INSERT INTO `liste_seance`(`nom_cinema`, `num_salle`, `num_seance`, `heure_debut`, `acces_seance`, `id_film`, `id_spectateur`) VALUES (@nom_cinema, @num_salle, @num_seance, @heure_debut, @acces_seance, @id_film, @id_spectateur)";
-            command2.Parameters.AddWithValue("@nom_cinema", nom_cinema);
-            command2.Parameters.AddWithValue("@num_salle", num_salle);
-            command2.Parameters.AddWithValue("@num_seance", num_seance);
-            command2.Parameters.AddWithValue("@heure_debut", heure_debut);
-            command2.Parameters.AddWithValue("@acces_seance", acces_seance);
-            command2.Parameters.AddWithValue("@id_film", id_film);
-            command2.Parameters.AddWithValue("@id_spectateur", id_spectateur);
+
+            command2.Parameters.AddWithValue("@nom_cinema", nomCinemaSeance);
+            command2.Parameters.AddWithValue("@num_salle", numSalleSeance);
+            command2.Parameters.AddWithValue("@num_seance", numDeSeance);
+            command2.Parameters.AddWithValue("@heure_debut", heureDebutSeance);
+            command2.Parameters.AddWithValue("@acces_seance", accesSeance);
+            command2.Parameters.AddWithValue("@id_film", idFilmSeance);
+            command2.Parameters.AddWithValue("@id_spectateur", id_spectateurSeance);
+
             command2.ExecuteNonQuery();
             Console.WriteLine("Spectateur avec sa séance insérée dans la base !");
+        }
+
+        public void DeleteSpectatorToSeanceFromDb(string deleteSpectateurFromSeanceDb)
+        {
+
+            // Insérer un spectateur avec sa séance dans la table "seance" :
+            using var command2 = connection.CreateCommand(); command2.Parameters.AddWithValue("@deleteSpectateurFromSeanceDb", deleteSpectateurFromSeanceDb);
+            command2.CommandText = @"DELETE FROM `seance` WHERE id_spectateur = @deleteSpectateurFromSeanceDb";
+
+            command2.ExecuteNonQuery();
+            Console.WriteLine("\nVous avez bien supprimé votre spectateur et sa séance de la base.");
         }
 
         // Insérer un spectateur VIP dans la table "spectateur" :
@@ -98,7 +111,125 @@ namespace DbManager
         }
     */
 
+        // Voir les infos d'un film :
+        public List<string> VoirFilm(string titre)
+        {
+            List<string> detailsFilm = new List<string>();
 
 
+            using var command = connection.CreateCommand();
+            command.Parameters.AddWithValue("@titre", titre);
+            command.CommandText = @"SELECT * FROM `film` WHERE titre = @titre";
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows) // Vérfie si la Db retourne bien un ou plusieurs résultats
+            {
+                Console.WriteLine("Le film existe bien"); // Debug statement to verify rows are found
+                while (reader.Read())
+                {
+                    string details = reader.GetString(0);
+                    detailsFilm.Add(details);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Le film n'existe pas");
+            }
+
+            reader.Close();
+            return detailsFilm;
+        }
+
+        // Voir les infos d'une seance : 
+        public List<string> VoirInfosSeance(int numeroSeance)
+        {
+            List<string> detailsSeance = new List<string>();
+
+
+            using var command = connection.CreateCommand();
+            command.Parameters.AddWithValue("@numeroSeance", numeroSeance);
+            command.CommandText = @"SELECT * FROM `film` WHERE num_seance = @numeroSeance";
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows) // Vérfie si la Db retourne bien un ou plusieurs résultats
+            {
+                Console.WriteLine("Le film existe bien"); // Debug statement to verify rows are found
+                while (reader.Read())
+                {
+                    string details = reader.GetString(0);
+                    detailsSeance.Add(details);
+                }
+            }
+            else
+            {
+                Console.WriteLine("La séance que vous cherchez n'est pas encore programmée");
+            }
+
+            reader.Close();
+            return detailsSeance;
+        }
+
+        // Voir si une réservation a bien été faite
+
+        public void VerifierReservationSeance(string nomSpectateur)
+        {
+
+            using var command = connection.CreateCommand();
+            command.Parameters.AddWithValue("@nomSpectateur", nomSpectateur);
+            // Requête avec jointure pour que le client puisse retrouver dans quelle séance il a réservé en tapant son nom
+            command.CommandText = "SELECT * FROM seance INNER JOIN spectateurs ON seance.id_spectateur = spectateurs.id_spectateur WHERE spectateurs.nom = @nomSpectateur";
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows) // Vérfie si la Db retourne bien un ou plusieurs résultats
+            {
+                Console.WriteLine("Requête dans la database réussie !"); // Debug statement to verify rows are found
+                while (reader.Read())
+                {
+                    string details = reader.GetString(0);
+                    Console.WriteLine("Vous avez bien réservé !");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Il n'y a aucune réservation à votre nom.");
+            }
+
+            reader.Close();
+        }
+        public List<string> VoirSynopsis(string rechercheFilm)
+        {
+            List<string> synopsis = new List<string>();
+
+
+            using var command = connection.CreateCommand();
+            command.Parameters.AddWithValue("@rechercheFilm", rechercheFilm);
+            command.CommandText = @"SELECT  `description` FROM `film` WHERE titre = @rechercheFilm";
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows) // Vérfie si la Db retourne bien un ou plusieurs résultats
+            {
+                Console.WriteLine("Voici le synopsis du film recherché :\n"); // Debug statement to verify rows are found
+                while (reader.Read())
+                {
+                    string descriptionFilm = reader.GetString(0);
+                    synopsis.Add(descriptionFilm);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Infos indisponible ou bien le film n'est pas encore programmé.");
+            }
+
+            reader.Close();
+            return synopsis;
+        }
     }
+
+
+
+
 }
--- Program.cs
+++ Program.cs
@@ -12,10 +12,10 @@ switch (userChoice)
 {
     case "Manager":
         Console.WriteLine("Bonjour, que souhaitez-vous faire ? \n");
-        Console.WriteLine("- Ajouter un nouveau spectateur à une séance\n - Supprimer un spectateur d'une séance\n - Ajouter une séance \n - Ajouter une séance VIP");
+        Console.WriteLine("- Ajouter un nouveau spectateur dans la base\n - Supprimer un spectateur de la base\n - Ajouter une séance normale dans la liste \n - Ajouter une séance VIP dans la liste \n - Voir les infos d'un film");
         string managerChoice = Console.ReadLine();
 
-        if (managerChoice == "Ajouter un nouveau spectateur à une séance")
+        if (managerChoice == "Ajouter un nouveau spectateur à la base")
         {
             Console.WriteLine("Quel est l'id du spectateur : ");
             int id_spectateur = int.Parse(Console.ReadLine());
@@ -30,25 +30,112 @@ switch (userChoice)
             dbPath.AddSpectateurToDb(id_spectateur, nom, prenom, statut);
 
         }
-        else if (managerChoice == "Supprimer un spectateur d'une séance")
+        else if (managerChoice == "Supprimer un spectateur de la base")
         {
             Console.WriteLine("Veuillez saisir le nom du spectateur que vous souhaitez supprimer : ");
             string deleteSpectateur = Console.ReadLine();
-            dbPath.DeleteVisitor(deleteSpectateur);
+            dbPath.DeleteSpectatorFromDB(deleteSpectateur);
         }
 
-        else if (managerChoice == "Ajouter une séance VIP")
+        /* else if (managerChoice == "Ajouter une séance VIP dans la liste")
         {
         
         }
+        */
 
-        else if (managerChoice == "Ajouter une séance" ){
-
+        else if (managerChoice == "Ajouter une séance normale dans la liste")
+        {
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            string nom_cinema = Console.ReadLine();
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            int num_salle = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            int num_seance = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            string heure_debut = Console.ReadLine();
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            string acces_seance = Console.ReadLine();
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            int id_film = int.Parse(Console.ReadLine());
+            // Appel de la fonction 
+            dbPath.AddSeanceToDb(nom_cinema, num_salle, num_seance, heure_debut, acces_seance, id_film);
         }
 
-        else if (managerChoice == "Voir les infos d'une séance"){
-            // Ajouter une condition afin de vérifier si le spectateur est VIP ou non 
+        else if (managerChoice == "Voir les infos d'un film")
+        {
+            Console.WriteLine("Veuillez saisir le nom du film que vous chezchez : ");
+            string titre = Console.ReadLine();
+            dbPath.VoirFilm(titre);
         }
+
+        break;
+
+    case "Staff":
+        Console.WriteLine("Bonjour, que souhaitez-vous faire ? \n");
+        Console.WriteLine("- Ajouter un nouveau spectateur à une séance\n - Supprimer un spectateur d'une séance\n - Voir les infos d'une seance");
+        string staffChoice = Console.ReadLine();
+
+        if (staffChoice == "Ajouter un nouveau spectateur à une séance")
+        {
+
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            string nomCinemaSeance = Console.ReadLine();
+            Console.WriteLine("Veuillez saisir le numéro de la salle de la séance : ");
+            int numSalleSeance = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez saisir le nom du cinéma où se déroule la séance : ");
+            int numDeSeance = int.Parse(Console.ReadLine());
+
+            string heureDebutSeance = Console.ReadLine();
+            string accesSeance = Console.ReadLine();
+            int idFilmSeance = int.Parse(Console.ReadLine());
+            int id_spectateurSeance = int.Parse(Console.ReadLine());
+
+            dbPath.AddSpectateurToSeanceDb(nomCinemaSeance, numSalleSeance, numDeSeance, heureDebutSeance, accesSeance, idFilmSeance, id_spectateurSeance);
+        }
+        else if (staffChoice == "Supprimer un spectateur d'une séance")
+        {
+            Console.WriteLine("Veuillez saisir l'id du spectateur que vous voulez supprimer : ");
+            string deleteSpectateurFromSeanceDb = Console.ReadLine();
+
+            // Appel de la méthode DeleteSpectatorToSeanceFromDb
+            dbPath.DeleteSpectatorToSeanceFromDb(deleteSpectateurFromSeanceDb);
+        }
+
+        else if (staffChoice == "Voir les infos d'une seance")
+        {
+            Console.WriteLine("Veuillez saisir le numéro de la séance que vous voulez regarder : ");
+            int numeroSeance = int.Parse(Console.ReadLine());
+
+            // Appel de la méthode VoirInfosSeance()
+            dbPath.VoirInfosSeance(numeroSeance);
+        }
+        break;
+
+    case "Spectateur":
+        Console.WriteLine("Bienvenue sur notre plateforme cher spectateur ! Que souhaitez-vous faire ?");
+        Console.WriteLine("- Vérifier ma réservation \n Voir le synopsis d'un film \n ");
+        string viewerChoice = Console.ReadLine();
+
+        if(viewerChoice == "Vérifier ma réservation"){
+            Console.WriteLine("A quel nom avez-vous effectué la réservation ? : ");
+            string nomSpectateur = Console.ReadLine();
+            // Appel de la méthode VerifierReservationSeance()
+            dbPath.VerifierReservationSeance(nomSpectateur);
+        }
+
+        else if (viewerChoice == "Voir le synopsis d'un film"){
+            Console.WriteLine("Quel est le titre du film que vous recherchez ?");
+            string rechercheFilm = Console.ReadLine();
+            // Appel de la méthode VoirSynopsis()
+            dbPath.VoirSynopsis(rechercheFilm);
+        }
+
+        break;
+
+    default: 
+
+    Console.WriteLine("Désolé, vous devez saisir un des 3 choix au-dessus...");
 
     break;
+
 }
